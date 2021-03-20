098b921af0c3d1ad06ae7598c9a5883d81f33b10
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-03-20 14:53:00 -0300

    feat: generate gifs-page component

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GifsPageComponent } from './gifs-page/gifs-page.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [GifsPageComponent],
   imports: [
     CommonModule
   ]
